65883cd0d6ad1de4f2eaa9e00a0a6231dd7807ee
Muhammad Zaman <mh.zaman.4069@gmail.com>
2023-02-19 16:17:26 -0500

    add middleware

--- Front-end/src/functions/GetThreads.js
+++ Front-end/src/functions/GetThreads.js
@@ -25,7 +25,15 @@ const DUMMY_THREADS = [
 ];
 
 const GetThreads = () => {
-    return DUMMY_THREADS;
+
+    fetch('http://localhost:8080/api/threads/all')
+    .then(response => {
+        return response.join();
+    })
+    .catch((error) => {
+        return DUMMY_THREADS;
+      }
+    )
 };
 
 export default GetThreads;
